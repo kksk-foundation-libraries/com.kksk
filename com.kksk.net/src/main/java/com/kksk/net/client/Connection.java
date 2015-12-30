f57095d54978dc70a747d9a07120259171bcfb2a
kakusuke-maruno <kakusuke.maruno@gmail.com>
2015-12-31 00:44:41 +0900

    brush up

--- com.kksk.net/src/main/java/com/kksk/net/client/Connection.java
+++ com.kksk.net/src/main/java/com/kksk/net/client/Connection.java
@@ -5,36 +5,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Calendar;
-import java.util.concurrent.atomic.AtomicLong;
 
+import com.kksk.identify.IdGenerator;
 import com.kksk.net.BinaryTranslator;
 
 public class Connection implements Closeable {
-	private static final long BIT_MASK = (1L << 31) - 1;
-	private static final long INT_MASK = (1L << 32) - 1;
 	private final Socket socket;
 	private final InputStream is;
 	private final OutputStream os;
 	protected final long id;
-	private static final AtomicLong sequence = new AtomicLong();
-	private static final long epoch;
-
-	static {
-		Calendar cal = Calendar.getInstance();
-		cal.set(2015, 0, 1, 0, 0, 0);
-		epoch = cal.getTimeInMillis();
-	}
 
 	public static long generateId() {
-		long localId = 0;
-		long tm = System.currentTimeMillis() - epoch;
-		if (sequence.compareAndSet(Integer.MAX_VALUE, 0)) {
-			tm += 1;
-		}
-		localId = (tm & BIT_MASK) << 32;
-		localId |= (sequence.getAndIncrement() & INT_MASK);
-		return localId;
+		return IdGenerator.generate(0);
 	}
 
 	protected Connection(Socket socket) throws IOException {
